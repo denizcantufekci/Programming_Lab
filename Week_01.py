39c6a2d3bd3ab8b6f4e178fb5ffef25d3f7a007f
Deniz Can Tüfekçi <denizcantufekci@gmail.com>
2020-03-07 15:40:04 +0300

    sorting algorithms added.

--- Week_01.py
+++ Week_01.py
@@ -52,3 +52,30 @@ def findMaxRange_ii(inlist=[4, -3, 5, -2, -1, 2, 6, -2]):   #Less control than f
                 max = t
                 i_1, i_2 = i, j
     return max , i_1, i_2
+
+#Some sort Functions
+
+def Selection(inList=[7, 6, 8, 9, 3, 2, 10, 5, 1]):
+    n = len(inList)
+    swaps, comparison = 0, 0
+    for key in range(n-1):
+        for i in range(key+1,n):
+            comparison += 1
+            if inList[i] < inList[key]:
+                swaps += 1
+                inList[i], inList[key] = inList[key], inList[i]
+    return 'SelectionSorted List: {} \nComparison: {} \nSwap: {} '.format(
+        inList, comparison, swaps)
+
+def Bubble(inList=[7, 6, 8, 9, 3, 2, 10, 5, 1]):
+    n = len(inList)
+    swaps, comparison = 0, 0
+    for i in range(n):
+        for j in range(0, n-i-1):
+            comparison += 1
+            if inList[j] > inList[j+1]:
+                swaps += 1
+                inList[j], inList[j+1] = inList[j+1], inList[j]
+
+    return 'BubbleSorted List: {} \nComparison: {} \nSwap: {} '.format(
+        inList, comparison, swaps)
