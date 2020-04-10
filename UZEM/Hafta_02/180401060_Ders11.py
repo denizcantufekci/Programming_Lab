a00e99d2f78247db77d4de4b6e97c242b98091fa
Deniz Can Tüfekçi <denizcantufekci@gmail.com>
2020-04-11 01:02:16 +0300

    Update 180401060_Ders11.py

--- UZEM/Hafta_02/180401060_Ders11.py
+++ UZEM/Hafta_02/180401060_Ders11.py
@@ -1,1 +1,16 @@
-# First Commit
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {
+    "collapsed": false
+   },
+   "outputs": [],
+   "source": [
+    "from sympy import Symbol, pprint",
+    "import sympy as sym",
+    "import sympy.plotting as syp"
+    "import matplotlib.pyplot as plt",
+    "%matplotlib notebook",
+    "%matplotlib inline"
+   ]
+  }
